48cd23843af321183117e39b1acd4a75cd588477
Nadeekakdu <151503941+nadeekath-kdu@users.noreply.github.com>
2025-03-04 10:28:32 +0530

    modify map risk level

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def predict_risk(data: RiskInput):
         probability_cvd = float(prediction_proba[1])
 
         # Map probability to risk level
-        risk_level = map_risk_level(probability_cvd)
+        risk_level = map_risk_level(probability_cvd - 0.5)
 
         # Return the results
         return {
